0bf12b4f0c7c3e079f6fa0b483605359734312f0
mgd <mr.zizikk@gamil.com>
2023-06-26 14:21:45 -0400

    refactoring admin panel mapping models

--- src/chain_store/admin.py
+++ src/chain_store/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from chain_store.models import (
     Contact,
@@ -9,14 +11,39 @@ from chain_store.models import (
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('email', 'full_address',)
+    list_filter = ('email', 'city', 'country', 'street',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'model', 'release_date',)
+    list_filter = ('title', 'model', 'release_date',)
 
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'title',
+        'contact',
+        'provider_link',
+        'debt',
+        'at_created',
+    )
+    list_filter = ('contact__city',)
+    actions = ('clear_debt',)
+
+    @admin.display(description='Поставщик')
+    def provider_link(self, obj):
+        provider = obj.provider
+
+        if provider:
+            url = reverse('admin:chain_store_store_change', args=[provider.id])
+
+            return format_html(f'<a href={url}>{provider.title}</a>')
+
+        return 'Нет поставщика'
+
+    @admin.action(description='Очистить задолженность перед поставщиком')
+    def clear_debt(self, request, queryset):
+        queryset.update(debt=0)
