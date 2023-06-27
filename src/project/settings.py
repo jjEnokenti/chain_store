a55c9c76ffff07e4378ff3dae1456c5b2d9de42b
mgd <mr.zizikk@gamil.com>
2023-06-27 00:04:29 -0400

    rename project config folder electronics > project

--- src/project/settings.py
+++ src/project/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'electronics.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'electronics.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
