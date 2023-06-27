38dd04b69c5a02514819c0163e38a240a82d137d
mgd <mr.zizikk@gamil.com>
2023-06-27 18:57:49 -0400

    add docs to product viewset

--- src/chain_store/views/product.py
+++ src/chain_store/views/product.py
@@ -1,3 +1,7 @@
+from drf_spectacular.utils import (
+    extend_schema,
+    extend_schema_view,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -5,6 +9,32 @@ from chain_store.models import Product
 from chain_store.serializers.product import ProductSerializer
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description='Response list of products',
+        summary='get list of products',
+    ),
+    retrieve=extend_schema(
+        description='Response detail of product',
+        summary='get detail of product',
+    ),
+    create=extend_schema(
+        description='Create new product',
+        summary='create new product',
+    ),
+    update=extend_schema(
+        description='Update product',
+        summary='update product',
+    ),
+    partial_update=extend_schema(
+        description='Partial update product',
+        summary='partial update product',
+    ),
+    destroy=extend_schema(
+        description='Delete product',
+        summary='delete product',
+    ),
+)
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
