ce181186a15f2268af0fc046ddc990842a3c004e
mgd <mr.zizikk@gamil.com>
2023-06-27 18:56:19 -0400

    add docs to contact viewset

--- src/chain_store/views/contact.py
+++ src/chain_store/views/contact.py
@@ -1,3 +1,7 @@
+from drf_spectacular.utils import (
+    extend_schema,
+    extend_schema_view,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -5,6 +9,32 @@ from chain_store.models import Contact
 from chain_store.serializers.contact import ContactSerializer
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description='Response list of contacts',
+        summary='get list of contacts',
+    ),
+    retrieve=extend_schema(
+        description='Response detail of contact',
+        summary='get detail of contact',
+    ),
+    create=extend_schema(
+        description='Create new contact',
+        summary='create new contact',
+    ),
+    update=extend_schema(
+        description='Update contact',
+        summary='update contact',
+    ),
+    partial_update=extend_schema(
+        description='Partial update contact',
+        summary='partial update contact',
+    ),
+    destroy=extend_schema(
+        description='Delete contact',
+        summary='delete contact',
+    ),
+)
 class ContactViewSet(ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
