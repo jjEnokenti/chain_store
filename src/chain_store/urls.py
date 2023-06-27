94f9bfc9c5c28ff552802ac13d800fb6dad52d83
mgd <mr.zizikk@gamil.com>
2023-06-27 18:01:50 -0400

    refactoring urls basename param

--- src/chain_store/urls.py
+++ src/chain_store/urls.py
@@ -11,9 +11,9 @@ router_provider = SimpleRouter()
 router_product = SimpleRouter()
 router_contact = SimpleRouter()
 
-router_provider.register('providers', provider.ProviderViewSet, basename='providers')
-router_product.register('products', provider.ProviderViewSet, basename='products')
-router_contact.register('contacts', provider.ProviderViewSet, basename='contacts')
+router_provider.register('providers', provider.ProviderViewSet, basename='provider')
+router_product.register('products', provider.ProviderViewSet, basename='product')
+router_contact.register('contacts', provider.ProviderViewSet, basename='contact')
 
 urlpatterns = [
     path('', include(router_provider.urls)),
