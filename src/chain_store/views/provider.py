35afe2718ffeb9b23557d270c94db2dbbdbbd9ff
mgd <mr.zizikk@gamil.com>
2023-06-27 18:58:42 -0400

    add docs to provider viewset

--- src/chain_store/views/provider.py
+++ src/chain_store/views/provider.py
@@ -1,4 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_spectacular.utils import (
+    extend_schema,
+    extend_schema_view,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -10,6 +14,32 @@ from chain_store.serializers.provider import (
 )
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description='Response list of providers',
+        summary='get list of providers',
+    ),
+    retrieve=extend_schema(
+        description='Response detail of provider',
+        summary='get detail of provider',
+    ),
+    create=extend_schema(
+        description='Create new provider',
+        summary='create new provider',
+    ),
+    update=extend_schema(
+        description='Update provider',
+        summary='update provider',
+    ),
+    partial_update=extend_schema(
+        description='Partial update provider',
+        summary='partial update provider',
+    ),
+    destroy=extend_schema(
+        description='Delete provider',
+        summary='delete provider',
+    ),
+)
 class ProviderViewSet(ModelViewSet):
     queryset = Provider.objects.all()
     serializer_default = ProviderSerializer
