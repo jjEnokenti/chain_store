79892ff606f3b628aa6f7b0ca9ad4868810289b0
mgd <mr.zizikk@gamil.com>
2023-06-27 19:23:37 -0400

    add docs to fixtures

--- src/tests/fixtures.py
+++ src/tests/fixtures.py
@@ -5,27 +5,32 @@ from tests import factories
 
 @pytest.fixture
 def auth_client(client, employee):
+    """authenticated client fixture."""
     client.force_login(employee)
     return client
 
 
 @pytest.fixture
 def employee(db):
+    """Employee fixture."""
     return factories.EmployeeFactory.create()
 
 
 @pytest.fixture
 def list_providers():
+    """List of providers fixture."""
     return factories.ProviderFactory.create_batch(size=5)
 
 
 @pytest.fixture
 def provider_factory():
+    """Provider factory fixture."""
     return factories.ProviderFactory
 
 
 @pytest.fixture
 def provider_level_0():
+    """Provider fabric fixture."""
     return factories.ProviderFactory.create(
         provider=None,
         products=(factories.ProductFactory.create_batch(size=5))
@@ -34,6 +39,7 @@ def provider_level_0():
 
 @pytest.fixture
 def provider_level_1(provider_level_0):
+    """Provider retail store fixture."""
     return factories.ProviderFactory.create(
         provider=provider_level_0,
         products=(factories.ProductFactory.create_batch(size=5))
@@ -42,6 +48,7 @@ def provider_level_1(provider_level_0):
 
 @pytest.fixture
 def provider_level_2_with_debt(provider_level_1):
+    """Provider individual entrepreneur fixture."""
     return factories.ProviderFactory.create(
         level=2,
         debt='123.32',
