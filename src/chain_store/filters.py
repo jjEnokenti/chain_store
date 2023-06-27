211c69afb58a497581ae60f4f7db30706f7f12e1
mgd <mr.zizikk@gamil.com>
2023-06-27 19:00:40 -0400

    add docs to custom filter and model

--- src/chain_store/filters.py
+++ src/chain_store/filters.py
@@ -4,6 +4,7 @@ from chain_store.models import Provider
 
 
 class ProviderFilter(fil.FilterSet):
+    """Filter by country fo providers"""
     country = fil.CharFilter(field_name='contact__country', lookup_expr='icontains')
 
     class Meta:
