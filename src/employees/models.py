5b81f0fcc4a57696894d3c9081fcb62f72cfe0ef
mgd <mr.zizikk@gamil.com>
2023-06-27 13:28:00 -0400

    add meta and ovverride __str__ method

--- src/employees/models.py
+++ src/employees/models.py
@@ -3,3 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 class Employee(AbstractUser):
     pass
+
+    class Meta:
+        verbose_name = ''
+        verbose_name_plural = ''
+
+    def __str__(self):
+        return self.username
