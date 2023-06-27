39f8d3e197c74bb6c3dc60d264bca9e302a9fcaf
mgd <mr.zizikk@gamil.com>
2023-06-27 18:35:46 -0400

    install drf-spectacular, configure and connect it

--- src/project/urls.py
+++ src/project/urls.py
@@ -3,10 +3,18 @@ from django.urls import (
     include,
     path,
 )
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/chain/', include('chain_store.urls')),
     path('api/employees/', include('employees.urls')),
+
+    # swagger
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
